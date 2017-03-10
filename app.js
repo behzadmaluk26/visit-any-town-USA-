8c87412d7ad12dbc8f0869f3b5c00191dc36f5a3
behzadmaluk26 <behzadmaluk@gmail.com>
2017-03-10 10:57:49 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var app =angular.module("angularApp",[]);
+
+ function onLoad() {
+     // this is an example of javascript alret function.
+                //alert("funtion onload() called");
+                document.getElementById("timestamp").innerHTML = Date();
+            }
